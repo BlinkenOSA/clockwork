d97625b8ad68bb3f7d003a1a35381f9c3cf9a2cf
JoshBone <jozsef.bone@gmail.com>
2017-11-07 15:50:22 +0100

    [Fix #10] Corrected title display in templates.

--- mlr/static/mlr/js/mlr.js
+++ mlr/static/mlr/js/mlr.js
@@ -10,24 +10,8 @@ var table = $('#mlr_table').DataTable({
             { "data": "building", "sortable": false },
             { "data": "mrss", "sortable": false },
 		    { "data": "action", "width": "100px", "sortable": false, "class": "action_column" }
-    ],
+    ]
 });
-
-$("#mlr_table").find("tfoot th").each( function ( i ) {
-	var select = $('<select><option value=""></option></select>')
-		.appendTo( $(this).empty() )
-		.on( 'change', function () {
-			var val = $(this).val();
-
-			table.column( i )
-				.search( val ? '^'+$(this).val()+'$' : val, true, false )
-				.draw();
-		} );
-
-	table.column( i ).data().unique().sort().each( function ( d, j ) {
-		select.append( '<option value="'+d+'">'+d+'</option>' );
-	} );
-} );
 
 $(function() {
 	$.fm({
