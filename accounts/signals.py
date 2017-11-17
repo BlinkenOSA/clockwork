82d66479dcb5b7ef99f817855866caa559a49b73
JoshBone <jozsef.bone@gmail.com>
2017-11-17 12:54:09 +0100

    [Fix #12] Added signal to create UserProfile.

---
+++ accounts/signals.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from accounts.models import UserProfile
+
+
+@receiver(post_save, sender=User, dispatch_uid='user.created')
+def set_user_permissions_upon_create(sender, instance, created, raw, using, **kwargs):
+    """ Adds 'change_profile' permission to created user objects """
+    if created:
+        from guardian.shortcuts import assign_perm
+        new_user_profile = UserProfile.objects.create(user=instance)
+        new_user_profile.save()
+        assign_perm('change_profile', instance, instance.user_profile)
