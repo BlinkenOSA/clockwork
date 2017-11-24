3cfd087870f4873e213a21d11be42095bccedc2a
JoshBone <jozsef.bone@gmail.com>
2017-11-24 14:41:35 +0100

    Set STATIC_ROOT in settings.

--- clockwork/components/production_settings.py
+++ clockwork/components/production_settings.py
@@ -9,10 +9,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['ams.osaarchivum.org']
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'public', 'media')
