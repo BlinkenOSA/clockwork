60f93ed4c72535b2244e46c7d0be5bff174ecab8
JoshBone <jozsef.bone@gmail.com>
2017-11-25 10:13:36 +0100

    Remember sidebar state.

--- static/clockwork/js/clockwork.js
+++ static/clockwork/js/clockwork.js
@@ -36,3 +36,40 @@ function displayMessage(message) {
     alertify.logPosition("bottom left");
 	alertify.delay(5000).success(message);
 }
+// AdminLTE sidebar hack
+$.AdminLTESidebarTweak = {};
+
+$.AdminLTESidebarTweak.options = {
+        EnableRemember: true,
+        NoTransitionAfterReload: true
+        //Removes the transition after page reload.
+};
+
+$(function () {
+    "use strict";
+
+    $("body").on("collapsed.pushMenu", function() {
+        if($.AdminLTESidebarTweak.options.EnableRemember) {
+            localStorage.setItem("toggleState", "closed");
+        }
+    });
+
+    $("body").on("expanded.pushMenu", function() {
+            if($.AdminLTESidebarTweak.options.EnableRemember) {
+                localStorage.setItem("toggleState", "opened");
+            }
+    });
+
+    if ($.AdminLTESidebarTweak.options.EnableRemember) {
+        var toggleState = localStorage.getItem("toggleState");
+        if (toggleState == 'closed'){
+            if ($.AdminLTESidebarTweak.options.NoTransitionAfterReload) {
+                $("body").addClass('sidebar-collapse hold-transition').delay(100).queue(function() {
+                    $(this).removeClass('hold-transition');
+                });
+            } else {
+                $("body").addClass('sidebar-collapse');
+            }
+        }
+    }
+});
