c6be12f1193811348875ccc14f3a52b5089a3f2b
JoshBone <jozsef.bone@gmail.com>
2018-04-09 17:20:50 +0200

    Added test_views for Accession module

--- accession/tests/test_views.py
+++ accession/tests/test_views.py
@@ -1,12 +1,13 @@
 import datetime
 import json
+import urllib
 
-from django.contrib.auth.models import User, Permission
+from django.contrib.auth.models import User
 from django.test import TestCase, RequestFactory
 from django.urls import reverse
 
 from accession.models import AccessionMethod, Accession
-from accession.views import AccessionListJson
+from accession.views import AccessionListJson, AccessionCreate
 from authority.models import Country
 from donor.models import Donor
 
@@ -30,33 +31,42 @@ class AccessionModelTest(TestCase):
             city='Budapest',
             address='Arany Janos u. 32.',
         )
-        self.accession = Accession.objects.create(
+        accession1 = Accession.objects.create(
             seq=1,
             title='Accession #1',
             transfer_date=datetime.datetime.now(),
             method=AccessionMethod.objects.get(pk=1),
             donor=donor
         )
+        accession2 = Accession.objects.create(
+            seq=2,
+            title='Test',
+            transfer_date=datetime.datetime.now(),
+            method=AccessionMethod.objects.get(pk=1),
+            donor=donor
+        )
+
         self.factory = RequestFactory()
 
     def test_accession_list_filter_queryset_with_search(self):
-        request = self.factory.get("%s?search=Accession" % reverse('accession:list_json'))
+        request = self.factory.get(reverse('accession:list_json'), {'search[value]': 'Accession #1'})
         request.user = self.user
         response = AccessionListJson.as_view()(request)
         response_json = json.loads(response.content)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response_json['recordsTotal'], 1)
+        self.assertEqual(response_json['recordsTotal'], 2)
+        self.assertEqual(response_json['recordsFiltered'], 1)
 
     def test_accession_list_filter_queryset_without_search(self):
-        request = self.factory.get("%s" % reverse('accession:list_json'))
+        request = self.factory.get(reverse('accession:list_json'))
         request.user = self.user
         response = AccessionListJson.as_view()(request)
         response_json = json.loads(response.content)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response_json['recordsTotal'], 1)
+        self.assertEqual(response_json['recordsTotal'], 2)
 
     def test_accession_list_render_column_transfer_date(self):
-        request = self.factory.get("%s" % reverse('accession:list_json'))
+        request = self.factory.get(reverse('accession:list_json'))
         request.user = self.user
         response = AccessionListJson.as_view()(request)
         response_json = json.loads(response.content)
@@ -64,14 +74,15 @@ class AccessionModelTest(TestCase):
         self.assertEqual(response_json['data'][0][1], datetime.datetime.now().strftime('%Y-%m-%d'))
 
     def test_accession_list_render_column_action(self):
-        request = self.factory.get("%s" % reverse('accession:list_json'))
+        request = self.factory.get(reverse('accession:list_json'))
         request.user = self.user
         response = AccessionListJson.as_view()(request)
         response_json = json.loads(response.content)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response_json['data'][0][3][1:4], 'div')
 
-    def add_permissions(self, group, ct):
-        perms = Permission.objects.filter(content_type=ct)
-        for p in perms:
-            group.permissions.add(p)
+    def test_accession_create_get_initial(self):
+        request = self.factory.get(reverse('accession:create'))
+        request.user = self.user
+        response = AccessionCreate.as_view()(request)
+        self.assertEqual(response.context_data['form'].initial['seq'], '%s/003' % datetime.datetime.now().strftime('%Y'))
