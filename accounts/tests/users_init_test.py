3a76733607d6d6310c31200e20efbc4432ba97d1
JoshBone <jozsef.bone@gmail.com>
2018-05-15 13:40:57 +0200

    remove unused tests

--- accounts/tests/users_init_test.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from userena.models import UserenaSignup
-
-
-class ModelTests(TestCase):
-
-    def setUp(self):
-        # Setup userena permissions
-        UserenaSignup.objects.check_permissions()
