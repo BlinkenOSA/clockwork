03d85968720d7ccb7e9356d68e6f48fed9af5b80
JoshBone <jozsef.bone@gmail.com>
2018-09-18 14:47:34 +0200

    Correct barcode in URL

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from api.views import GetSetDigitizedContainer, GetContainerMetadata
 
 urlpatterns = [
-    url(r'^containers/(?P<barcode>\d+)/$', GetSetDigitizedContainer.as_view(), name='list_set_digitized_container'),
-    url(r'^containers/metadata/(?P<barcode>\d+)/$', GetContainerMetadata.as_view(), name='list_get_container_metadata'),
+    url(r'^containers/(?P<barcode>\w+)/$', GetSetDigitizedContainer.as_view(), name='list_set_digitized_container'),
+    url(r'^containers/metadata/(?P<barcode>\w+)/$', GetContainerMetadata.as_view(), name='list_get_container_metadata'),
 ]
