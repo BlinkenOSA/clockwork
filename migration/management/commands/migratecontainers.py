3707a20609bb36cfe5481c632219ac364327582e
JoshBone <jozsef.bone@gmail.com>
2019-01-25 11:15:04 +0100

    Include Fonds, Subfonds, Series selection options in the migration scripts.

--- migration/management/commands/migratecontainers.py
+++ migration/management/commands/migratecontainers.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 import mysql.connector
-from django.core.management import BaseCommand
+from django.core.management import BaseCommand, CommandError
 from django.conf import settings
 from django.db import IntegrityError
 from pytz import timezone
@@ -14,12 +14,33 @@ from migration.management.commands.common_functions import get_user
 class Command(BaseCommand):
     help = 'Migrate Containers.'
 
+    def add_arguments(self, parser):
+        parser.add_argument('--fonds', dest='fonds', help='Fonds Number')
+        parser.add_argument('--subfonds', dest='subfonds', help='Subfonds Number')
+        parser.add_argument('--series', dest='series', help='Series Number')
+        parser.add_argument('--container_list', dest='container_list', help='Container List')
+
     def handle(self, *args, **options):
         if settings.MIGRATION_DB:
             cnx = mysql.connector.connect(user=settings.MIGRATION_DB['USER'],
                                           password=settings.MIGRATION_DB['PASSWORD'],
                                           host=settings.MIGRATION_DB['HOST'],
                                           database=settings.MIGRATION_DB['DB'])
+            where = []
+
+            if options['fonds']:
+                where.append("Main.FondsID = %s" % options['fonds'])
+            if options['subfonds']:
+                where.append("Main.SubfondsID = %s" % options['subfonds'])
+            if options['series']:
+                where.append("Main.SeriesID = %s" % options['series'])
+
+            if options['container_list']:
+                where.append("Main.ListNo = %s" % options['container_list'])
+            else:
+                where.append("Main.ListNo = 1")
+
+            where = " AND ".join(where)
 
             cursor = cnx.cursor(dictionary=True, buffered=True)
             sql = "SELECT Main.ID, Main.FondsID, Main.SubfondsID, Main.SeriesID, Main.ListNo, ListsInSeries.Medium, " \
@@ -33,26 +54,26 @@ class Command(BaseCommand):
                   "Main.SubfondsID = ListsInSeries.SubfondsId AND " \
                   "Main.SeriesID = ListsInSeries.SeriesId AND " \
                   "Main.ListNo = ListsInSeries.ListNo " \
-                  "INNER JOIN Container ON Main.ContType = Container.ID " \
-                  "WHERE Main.ListNo = 1 AND Main.FondsID >= 0 " \
-                  "ORDER BY FondsID, SubfondsID, SeriesID, ListNo, Container"
+                  "INNER JOIN Container ON Main.ContType = Container.ID "
+
+            sql = sql + "WHERE " + where + " ORDER BY FondsID, SubfondsID, SeriesID, ListNo, Container"
 
             cursor.execute(sql)
 
             carrier_map = {
-                'Archival boxes': CarrierType.objects.get(type='Archival box'),
+                'Archival boxes': CarrierType.objects.get(type='Archival boxes'),
                 'Archival card boxes': CarrierType.objects.get(type='Archival card box'),
                 'Boxes of color prints': CarrierType.objects.get(type='Boxes of color prints'),
                 'Audio cassette': CarrierType.objects.get(type='Audio cassette'),
-                'Beta SP': CarrierType.objects.get(type='Beta SP'),
-                'VHS': CarrierType.objects.get(type='VHS'),
+                'Beta SP': CarrierType.objects.get(type='BetaSP PAL'),
+                'VHS': CarrierType.objects.get(type='VHS PAL'),
                 'CD-ROM': CarrierType.objects.get(type='CD-ROM'),
                 'Rolls of 35mm microfilm in microfilm cabinet':
                     CarrierType.objects.get(type='Rolls of 35 mm microfilm in microfilm cabinet'),
                 'Rolls of 16mm microfilm in microfilm cabinet':
                     CarrierType.objects.get(type='Rolls of 16 mm microfilm in microfilm cabinet'),
                 'SVHS': CarrierType.objects.get(type='SVHS'),
-                'Plastic card box': CarrierType.objects.get(type='VHS'),
+                'Plastic card box': CarrierType.objects.get(type='Plastic card box'),
                 "Floppy 3''": CarrierType.objects.get(type='Floppy 3.5'),
                 "Floppy 5''": CarrierType.objects.get(type='Floppy 5.25'),
                 'DVD-ROM': CarrierType.objects.get(type='DVD-ROM'),
