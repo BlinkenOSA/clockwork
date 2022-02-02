b3d8769b23dfcfd4f7489f22a022ad0462fe9360
Josh <jozsef.bone@gmail.com>
2022-02-02 12:44:01 +0100

    Added API endpoint for Research Cloud upload.

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import url
 
 from api.views import GetSetDigitizedContainer, GetContainerMetadata, FindingAidsEntityListView, \
-    FindingAidsEntityUpdateView
+    FindingAidsEntityUpdateView, GetContainerMetadataByLegacyID
 
 urlpatterns = [
     url(r'^containers/(?P<barcode>\w+)/$', GetSetDigitizedContainer.as_view(), name='list_set_digitized_container'),
     url(r'^containers/metadata/(?P<barcode>\w+)/$', GetContainerMetadata.as_view(), name='list_get_container_metadata'),
     url(r'^containers/technical_metadata/(?P<barcode>\w+)/$', GetContainerMetadata.as_view(), name='list_get_container_metadata'),
+
+    url(r'^containers/by-legacy-id/(?P<legacy_id>[\w\. -]+)/$', GetContainerMetadataByLegacyID.as_view(), name='get_container_by_legacy_id'),
 
     url(r'^finding_aids/list/(?P<series_id>\w+)/$', FindingAidsEntityListView.as_view(), name='list_finding_aids_view'),
     url(r'^finding_aids/(?P<pk>\d+)/$', FindingAidsEntityUpdateView.as_view(), name='update_finding_aids_view'),
