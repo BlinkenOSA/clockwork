c49934788785ea48585ab221d889d607b3ed84c8
Josh <jozsef.bone@gmail.com>
2022-02-03 11:11:05 +0100

    Include solr environment variable.

--- finding_aids/tasks.py
+++ finding_aids/tasks.py
@@ -1,13 +1,16 @@
 import pysolr
 from celery.task import task
 
+from django.conf import settings
+
 from finding_aids.indexer import FindingAidsEntityIndexer
 from finding_aids.models import FindingAidsEntity
 
 
 @task(name="index_add_finding_aids")
 def index_add_finding_aids(finding_aids_entity_id):
-    solr_interface = pysolr.Solr("http://localhost:8983/solr/osacatalog")
+    solr_core = getattr(settings, "SOLR_ADDRESS", "http://localhost:8983/solr/osacatalog")
+    solr_interface = pysolr.Solr(solr_core)
     finding_aids = FindingAidsEntity.objects.get(pk=finding_aids_entity_id)
     indexer = FindingAidsEntityIndexer(finding_aids_entity_id=finding_aids.id)
     indexer.prepare_index()
@@ -16,7 +19,8 @@ def index_add_finding_aids(finding_aids_entity_id):
 
 @task(name="index_add_finding_aids_confidential")
 def index_add_finding_aids_confidential(finding_aids_entity_id):
-    solr_interface = pysolr.Solr("http://localhost:8983/solr/osacatalog")
+    solr_core = getattr(settings, "SOLR_ADDRESS", "http://localhost:8983/solr/osacatalog")
+    solr_interface = pysolr.Solr(solr_core)
     finding_aids = FindingAidsEntity.objects.get(pk=finding_aids_entity_id)
     indexer = FindingAidsEntityIndexer(finding_aids_entity_id=finding_aids.id)
     indexer.prepare_confidential_index()
@@ -25,7 +29,8 @@ def index_add_finding_aids_confidential(finding_aids_entity_id):
 
 @task(name="index_remove_finding_aids")
 def index_remove_finding_aids(finding_aids_entity_id):
-    solr_interface = pysolr.Solr("http://localhost:8983/solr/osacatalog")
+    solr_core = getattr(settings, "SOLR_ADDRESS", "http://localhost:8983/solr/osacatalog")
+    solr_interface = pysolr.Solr(solr_core)
     finding_aids = FindingAidsEntity.objects.get(pk=finding_aids_entity_id)
     indexer = FindingAidsEntityIndexer(finding_aids_entity_id=finding_aids.id)
     solr_interface.delete(id=indexer.get_solr_id(), commit=True)
